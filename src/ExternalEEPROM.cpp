2a9d983a02a76d7ead516c36763d3e7e6f0317f9
teissem <teissedre.mathieu@outlook.com>
2023-07-10 08:50:22 +0200

    Version upgrade of ExternalEEPROM

--- src/ExternalEEPROM.cpp
+++ src/ExternalEEPROM.cpp
@@ -7,7 +7,7 @@ ExternalEEPROM::ExternalEEPROM(int i2c_address){
     this->i2c_address = i2c_address;
 }
 
-void ExternalEEPROM::write(int address, byte value){
+void ExternalEEPROM::write8bit(int address, byte value){
     // Switch case
     Wire.beginTransmission(this->i2c_address);
     Wire.write((int)(address >> 8));
@@ -18,7 +18,20 @@ void ExternalEEPROM::write(int address, byte value){
     delay(5);
 }
 
-byte ExternalEEPROM::read(int address){
+
+void ExternalEEPROM::writeInt(int address, int value){
+    for (int i=0; i<sizeof(int); i++)
+    {
+        // Write 8 LSB only
+        byte value_to_write = (byte) (value & 0xFF);
+        this->write8bit(address, value_to_write);
+        // Shift right and prepare for next loop
+        value >>= 8 ;
+        address++ ;
+    }
+}
+
+byte ExternalEEPROM::read8bit(int address){
     byte data = 0xFF;
     // Switch case
     Wire.beginTransmission(this->i2c_address);
@@ -30,4 +43,14 @@ byte ExternalEEPROM::read(int address){
     // Read data
     data = Wire.read();
     return data;
+}
+
+int ExternalEEPROM::readInt(int address) {
+    int value = 0;
+    for (int i = sizeof(int) - 1; i >= 0; i--) {
+        value += (int) this->read8bit(address + i);
+        value <<= 8;
+    }
+    value >>= 8;
+    return value;
 }
--- src/ExternalEEPROM.h
+++ src/ExternalEEPROM.h
@@ -8,8 +8,10 @@ class ExternalEEPROM {
         int i2c_address;
     public:
         ExternalEEPROM(int i2c_address);
-        void write(int address, byte value);
-        byte read(int address);
+        void write8bit(int address, byte value);
+        void writeInt(int address, int value);
+        byte read8bit(int address);
+        int readInt(int address);
 };
 
 #endif
